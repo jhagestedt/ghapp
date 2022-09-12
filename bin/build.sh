bc6f8e3ea5472a51fb9a77e47ecc91857a8294ba
jhagestedt <julien.hagestedt@gmail.com>
2022-09-12 14:27:54 +0200

    fix(ci): build with cgoenabled to false for alpine linux support

--- bin/build.sh
+++ bin/build.sh
@@ -6,10 +6,13 @@ OSS=(linux darwin)
 ARCHS=(amd64 arm64)
 
 go get
+go mod download
 
 for OS in "${OSS[@]}"; do
   for ARCH in "${ARCHS[@]}"; do
-    env GOOS="${OS}" GOARCH="${ARCH}" \
+    CGO_ENABLED=0 \
+    GOOS="${OS}" \
+    GOARCH="${ARCH}" \
     go build \
     -o "./dist/ghapp_${OS}_${ARCH}" \
     -ldflags "-X main.version=${VERSION}"
